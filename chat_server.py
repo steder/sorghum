7f089b24393d8a300a71bf39af43945364feaa3a
Mike Steder <steder@gmail.com>
2011-12-19 22:12:59 -0600

    Moving to json formatted messages.

--- chat_server.py
+++ chat_server.py
@@ -1,22 +1,29 @@
+import json
 import sys
-from twisted.internet import reactor
-from twisted.python import log
+
 from autobahn.websocket import (WebSocketServerFactory, WebSocketServerProtocol,
                                 listenWS)
- 
+from twisted.internet import reactor
+from twisted.python import log
+
  
 class BroadcastServerProtocol(WebSocketServerProtocol):
  
    def onOpen(self):
-      self.factory.register(self)
+       self.factory.register(self)
  
    def onMessage(self, msg, binary):
-      if not binary:
-         self.factory.broadcast("%s: %s" % (self.peerstr, msg))
+       if not binary:                    
+           data = json.loads(msg)
+           print "%s (%s): %s" % (data["nickname"], self.peerstr, data["message"])
+           response = json.dumps({"nickname":data["nickname"],
+                                  "ip":self.peerstr,
+                                  "message":data["message"]})
+           self.factory.broadcast(response)
  
    def connectionLost(self, reason):
-      WebSocketServerProtocol.connectionLost(self, reason)
-      self.factory.unregister(self)
+       WebSocketServerProtocol.connectionLost(self, reason)
+       self.factory.unregister(self)
  
  
 class BroadcastServerFactory(WebSocketServerFactory):
@@ -24,36 +31,35 @@ class BroadcastServerFactory(WebSocketServerFactory):
    protocol = BroadcastServerProtocol
  
    def __init__(self, url):
-      WebSocketServerFactory.__init__(self, url)
-      self.clients = []
-      self.tickcount = 0
-      self.tick()
+       WebSocketServerFactory.__init__(self, url)
+       self.clients = []
+       self.tickcount = 0
+       self.tick()
  
    def tick(self):
-      self.tickcount += 1
-      #self.broadcast("tick %d" % self.tickcount)
-      reactor.callLater(60, self.tick)
+       self.tickcount += 1
+       #self.broadcast("tick %d" % self.tickcount)
+       reactor.callLater(60, self.tick)
  
    def register(self, client):
-      if not client in self.clients:
-         print "registered client " + client.peerstr
-         self.clients.append(client)
+       if not client in self.clients:
+           print "registered client " + client.peerstr
+           self.clients.append(client)
  
    def unregister(self, client):
-      if client in self.clients:
-         print "unregistered client " + client.peerstr
-         self.clients.remove(client)
+       if client in self.clients:
+           print "unregistered client " + client.peerstr
+           self.clients.remove(client)
  
    def broadcast(self, msg):
-      print "broadcasting message '%s' .." % msg
-      for c in self.clients:
-         print "send to " + c.peerstr
-         c.sendMessage(msg)
+       print "broadcasting message '%s' .." % (msg,)
+       for c in self.clients:
+           print "sending to %s... " % (c.peerstr,)
+           c.sendMessage(msg.encode("utf-8"))
  
  
 if __name__ == '__main__':
- 
-   log.startLogging(sys.stdout)
-   factory = BroadcastServerFactory("ws://localhost:9000")
-   listenWS(factory)
-   reactor.run()
+    log.startLogging(sys.stdout)
+    factory = BroadcastServerFactory("ws://localhost:9000")
+    listenWS(factory)
+    reactor.run()
