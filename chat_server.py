70d48bafc85f0584f28502e4e5cc566908850d1a
Mike Steder <steder@gmail.com>
2011-12-29 19:21:56 -0600

    Adding example d6 command.

--- chat_server.py
+++ chat_server.py
@@ -1,4 +1,5 @@
 import json
+import random
 import sys
 
 from autobahn.websocket import (WebSocketServerFactory, WebSocketServerProtocol,
@@ -13,51 +14,58 @@ MESSAGE_TYPE_PART = 3
 
 
 class BroadcastServerProtocol(WebSocketServerProtocol):
- 
+
    def onOpen(self):
-       self.factory.register(self)
- 
+      self.factory.register(self)
+
    def onMessage(self, msg, binary):
-       if not binary:                    
-           data = json.loads(msg)
-           msg_type = data.get("type", MESSAGE_TYPE_CHAT)
-           if msg_type == MESSAGE_TYPE_JOIN:
-               response = json.dumps({"nickname":data["nickname"],
-                                      "userid":self.peerstr,
-                                      "type":MESSAGE_TYPE_JOIN})
-               self.factory.broadcast(response)
-           else:
-               print "%s (%s): %s" % (data["nickname"], self.peerstr, data["message"])
-               response = json.dumps({"nickname":data["nickname"],
-                                    "ip":self.peerstr,
-                                    "message":data["message"]})
-               self.factory.broadcast(response)
-               
+       if not binary:
+          data = json.loads(msg)
+          msg_type = data.get("type", MESSAGE_TYPE_CHAT)
+          if msg_type == MESSAGE_TYPE_JOIN:
+             response = json.dumps({"nickname":data["nickname"],
+                                    "userid":self.peerstr,
+                                    "type":MESSAGE_TYPE_JOIN})
+             self.factory.broadcast(response)
+          else:
+             message = data["message"]
+             if message.startswith("/d6"):
+                response = json.dumps({"nickname":"DM",
+                                       "ip":self.peerstr,
+                                       "message":"%s rolled a d6 and got %s"%(data["nickname"], random.randint(1, 6))})
+                self.factory.broadcast(response)
+             else:
+                print "%s (%s): %s" % (data["nickname"], self.peerstr, data["message"])
+                response = json.dumps({"nickname":data["nickname"],
+                                       "ip":self.peerstr,
+                                       "message":data["message"]})
+                self.factory.broadcast(response)
+
    def connectionLost(self, reason):
        WebSocketServerProtocol.connectionLost(self, reason)
        self.factory.unregister(self)
- 
- 
+
+
 class BroadcastServerFactory(WebSocketServerFactory):
- 
+
    protocol = BroadcastServerProtocol
- 
+
    def __init__(self, url):
        WebSocketServerFactory.__init__(self, url)
        self.clients = []
        self.tickcount = 0
        self.tick()
- 
+
    def tick(self):
        self.tickcount += 1
        #self.broadcast("tick %d" % self.tickcount)
        reactor.callLater(60, self.tick)
- 
+
    def register(self, client):
        if not client in self.clients:
            print "registered client " + client.peerstr
            self.clients.append(client)
- 
+
    def unregister(self, client):
        if client in self.clients:
            print "unregistered client " + client.peerstr
@@ -65,14 +73,14 @@ class BroadcastServerFactory(WebSocketServerFactory):
            partMessage = json.dumps({"userid":client.peerstr,
                                       "type":MESSAGE_TYPE_PART})
            self.broadcast(partMessage)
- 
+
    def broadcast(self, msg):
        print "broadcasting message '%s' .." % (msg,)
        for c in self.clients:
            print "sending to %s... " % (c.peerstr,)
            c.sendMessage(msg.encode("utf-8"))
- 
- 
+
+
 if __name__ == '__main__':
     log.startLogging(sys.stdout)
     factory = BroadcastServerFactory("ws://localhost:9000")
